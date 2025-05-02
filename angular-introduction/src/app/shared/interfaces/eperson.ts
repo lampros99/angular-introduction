35d7473fd31cc515e86c6031a56b8da8fb7c38e7
lampros alexandris <lamprospatmos19@gmail.com>
2025-05-02 10:22:52 +0300

    interfaces & data

---
+++ angular-introduction/src/app/shared/interfaces/eperson.ts
@@ -0,0 +1,101 @@
+export interface EPerson {
+  givenName: string;
+  surName: string;
+  age: string;
+  email: string;
+  education: string
+}
+
+export const ManyPerson: EPerson[] = [
+  {
+    givenName: 'Sarah',
+    surName: 'Howard',
+    age: '41',
+    email: 's.m.howard@yahoo.com',
+    education: 'Some college, no degree',
+  },
+  {
+    givenName: 'Alexander',
+    surName: 'Turner',
+    age: '43',
+    email: 'alexanderfturner@gmail.com',
+    education: 'Master’s degree',
+  },
+  {
+    givenName: 'Brianna',
+    surName: 'Russell',
+    age: '32',
+    email: 'b_g_russell@yahoo.com',
+    education: 'Doctoral degree',
+  },
+  {
+    givenName: 'Ashley',
+    surName: 'Ward',
+    age: '64',
+    email: 'amward@rocketmail.com',
+    education: 'Some college, no degree',
+  },
+  {
+    givenName: 'Alexandra',
+    surName: 'Campbell',
+    age: '76',
+    email: 'alexandracampbell@hotmail.com',
+    education: 'Less than high school',
+  },
+  {
+    givenName: 'Jonathan',
+    surName: 'Scott',
+    age: '26',
+    email: 'jonathanscott@ymail.com',
+    education: 'Less than high school',
+  },
+  {
+    givenName: 'Tyler',
+    surName: 'Moore',
+    age: '62',
+    email: 'tyler_j_moore@live.com',
+    education: 'Doctoral degree',
+  },
+  {
+    givenName: 'Laura',
+    surName: 'Johnson',
+    age: '58',
+    email: 'lrjohnson29@rocketmail.com',
+    education: 'Some college, no degree',
+  },
+  {
+    givenName: 'Melissa',
+    surName: 'Garcia',
+    age: '33',
+    email: 'mlgarcia77@gmail.com',
+    education: 'Less than high school',
+  },
+  {
+    givenName: 'Ethan',
+    surName: 'Hill',
+    age: '67',
+    email: 'e_m_hill@yahoo.com',
+    education: 'High school diploma or equivalent',
+  },
+  {
+    givenName: 'Samuel',
+    surName: 'Baker',
+    age: '51',
+    email: 's_baker@ymail.com',
+    education: 'Bachelor’s degree',
+  },
+  {
+    givenName: 'Charles',
+    surName: 'Powell',
+    age: '53',
+    email: 'charles.powell@gmail.com',
+    education: 'Less than high school',
+  },
+  {
+    givenName: 'Jeremy',
+    surName: 'Brown',
+    age: '62',
+    email: 'j.brown53@aol.com',
+    education: 'Master’s degree',
+  },
+];
