e07345c521849ad4348d8ebc6dee9975ebc704aa
lampros alexandris <lamprospatmos19@gmail.com>
2025-05-02 10:14:43 +0300

    example components

---
+++ angular-introduction/src/app/component-input-example/component-input-example.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Person } from '../shared/interfaces/person';
+import { PersonTabComponent } from '../components/person-tab/person-tab.component';
+
+@Component({
+  selector: 'app-component-input-example',
+  standalone: true,
+  imports: [PersonTabComponent],
+  templateUrl: './component-input-example.component.html',
+  styleUrls: ['./component-input-example.component.css']
+})
+export class ComponentInputExampleComponent {
+  person0: Person = {
+    givenName: 'Lampros',
+    surName: 'Alexandris',
+    age: 30,
+    email: 'ale@aueb.gr',
+    address: 'Athens, Greece'
+  };
+
+  person1: Person = {
+    givenName: 'George',
+    surName: 'Michail',
+    age: 40,
+    email: 'micha@aueb.gr',
+    address: 'Kalamata, Greece'
+  };
+}
