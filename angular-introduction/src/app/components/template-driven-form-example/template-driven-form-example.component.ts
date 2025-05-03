f2d627f389b9f5ef40dc4f1991496e1fec4dfe13
lampros alexandris <lamprospatmos19@gmail.com>
2025-05-03 22:34:20 +0300

    template-driven-form-example/

---
+++ angular-introduction/src/app/components/template-driven-form-example/template-driven-form-example.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { EpersonTemplateDrivenFormComponent } from '../eperson-template-driven-form/eperson-template-driven-form.component';
+import { PersonTabComponent } from '../person-tab/person-tab.component'; 
+import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
+import { EPerson } from 'src/app/shared/interfaces/eperson';
+
+@Component({
+  selector: 'app-template-driven-form-example',
+  imports: [EpersonTemplateDrivenFormComponent, PersonTabComponent, SimpleDatatableComponent],
+  templateUrl: './template-driven-form-example.component.html',
+  styleUrl: './template-driven-form-example.component.css'
+})
+export class TemplateDrivenFormExampleComponent {
+  // persons: EPerson[] = [];
+  // currentPerson: EPerson = {
+  //   givenName: '',
+  //   surName: '',
+  //   age: '',
+  //   email:'',
+  //   education:''
+  // };
+
+
+}
